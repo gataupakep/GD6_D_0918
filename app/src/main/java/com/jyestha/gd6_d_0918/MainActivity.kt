f575da719f2883e9642afb6873d4be92f4f3fd8f
Jyestha <jyestha47@gmail.com>
2022-09-24 08:51:35 +0700

    Menambah createActivity pada MainActivity.kt

--- app/src/main/java/com/jyestha/gd6_d_0918/MainActivity.kt
+++ app/src/main/java/com/jyestha/gd6_d_0918/MainActivity.kt
@@ -26,7 +26,6 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding!!.root)
 
-        createNotificationChannel()
         binding!!.btn1.setOnClickListener{
             sendNotification1()
         }
